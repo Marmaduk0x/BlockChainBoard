ca12b525ba6a1b55619c1485aeaa40bd44a47423
Marma__duk <160778212+Marmaduk0x@users.noreply.github.com>
2024-05-14 21:38:05 +0200

    clean code

--- useWeb3.ts
+++ useWeb3.ts
@@ -12,34 +12,47 @@ const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
 
 export const useWeb3 = () => {
   const [web3State, setWeb3State] = useState<Web3State>({});
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<Error | undefined>();
 
   useEffect(() => {
-    const loadWeb3 = async () => {
-      try {
-        setLoading(true);
-        if (window.ethereum) {
-          await window.ethereum.request({ method: 'eth_requestAccounts' });
-          const web3 = new Web3(window.ethereum);
-          const accounts = await web3.eth.getAccounts();
-
-          const contract = new web3.eth.Contract(BulletinBoardABI, CONTRACT_ADDRESS);
-
-          setWeb3State({ web3, accounts, contract });
-        } else {
-          console.error('Install MetaMask.');
-          setError(new Error('Web3 provider not found. Please install MetaMask.'));
-        }
-      } catch (e) {
-        setError(e as Error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    
-    loadWeb3();
+    initWeb3();
   }, []);
+
+  const initWeb3 = async () => {
+    try {
+      checkIfMetaMaskIsAvailable();
+      const web3 = initializeWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const contract = initializeContract(web3);
+      setWeb3State({ web3, accounts, contract });
+    } catch (error) {
+      handleError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const checkIfMetaMaskIsAvailable = () => {
+    if (!window.ethereum) {
+      const errorMessage = 'Web3 provider not found. Please install MetaMask.';
+      console.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+  };
+
+  const initializeWeb3 = () => {
+    return new Web3(window.ethereum);
+  };
+
+  const initializeContract = (web3: Web3) => {
+    return new web3.eth.Contract(BulletinBoardABI, CONTRACT_ADDRESS);
+  };
+
+  const handleError = (error: any) => {
+    console.error("An error occurred: ", error.message);
+    setError(new Error("Failed to load Web3, accounts, or contract. Check console for details."));
+  };
 
   return { ...web3State, loading, error };
 };
